0f8df7f0fbf3f95debeae1455a4d42e840651be6
Daniel Spiewak <djspiewak@gmail.com>
2011-08-21 21:18:20 -0500

    Implemented initProject and typeCompletion functions

--- src/main/scala/com/codecommit/es/EnsimeProtocolComponent.scala
+++ src/main/scala/com/codecommit/es/EnsimeProtocolComponent.scala
@@ -35,7 +35,9 @@ trait EnsimeProtocolComponent extends BackendComponent {
     private val lock = new AnyRef
     
     def connectionInfo(callback: ConnectionInfo => Unit) {
-      def parse(sexp: SExp) = sexp match {
+      val id = callId()
+      
+      registerReturn(id) {
         case SExpList(KeywordAtom(":pid") :: pidS :: KeywordAtom(":server-implementation") :: SExpList(KeywordAtom(":name") :: StringAtom(name) :: Nil) :: KeywordAtom(":machine") :: machineS :: KeywordAtom(":features") :: featuresS :: KeywordAtom(":version") :: StringAtom(version) :: Nil) => {
           val pid = pidS match {
             case NilAtom() => None
@@ -50,15 +52,40 @@ trait EnsimeProtocolComponent extends BackendComponent {
         }
       }
       
+      dispatchSwank(id, SExp(key("swank:connection-info")))
+    }
+    
+    def initProject(rootDir: String) {
+      dispatchSwank(callId(), SExp(key("swank:init-project"), propList(":root-dir" -> StringAtom(rootDir))))
+    }
+    
+    def typeCompletion(file: String, offset: Int, prefix: String)(callback: List[CompletionResult] => Unit) {
       val id = callId()
-      returnLock synchronized {
-        returns += (id -> parse)
+      
+      registerReturn(id) {
+        case SExpList(results) => {
+          val back = results map {
+            case props: SExpList => {
+              val map = props.toKeywordMap
+              
+              val StringAtom(name) = map(key(":name"))
+              val StringAtom(typeSig) = map(key(":type-sig"))
+              val IntAtom(typeId) = map(key(":type-id"))
+              val BooleanAtom(isCallable) = map.getOrElse(key(":is-callable"), TruthAtom())
+              
+              CompletionResult(name, typeSig, typeId, isCallable)
+            }
+          }
+          
+          callback(back.toList)
+        }
       }
-      dispatchSwank(id, SExpList(Symbol("swank:connection-info") :: Nil))
+      
+      dispatchSwank(id, SExp(key("swank:type-completion"), file, offset, prefix))
     }
     
     private def dispatchSwank(id: Int, sexp: SExp) {
-      Backend.send(SExpList(key(":swank-rpc") :: sexp :: IntAtom(id) :: Nil).toWireString)
+      Backend.send(SExp(key(":swank-rpc"), sexp, id).toWireString)
     }
     
     private def callId() = lock synchronized {
@@ -67,12 +94,23 @@ trait EnsimeProtocolComponent extends BackendComponent {
     }
   }
   
+  private def registerReturn(id: Int)(f: SExp => Unit) {
+    returnLock synchronized {
+      returns += (id -> f)
+    }
+  }
+  
   // TODO
   trait Ensime {
     def connectionInfo(callback: ConnectionInfo => Unit)
+    def initProject(rootDir: String)
+    
+    def typeCompletion(file: String, offset: Int, prefix: String)(callback: List[CompletionResult] => Unit)
   }
 }
 
 object EnsimeProtocol {
   case class ConnectionInfo(port: Option[Int], name: String, machine: Option[String], features: List[String], version: String)
+  
+  case class CompletionResult(name: String, typeSig: String, typeId: Int, isCallable: Boolean)
 }
