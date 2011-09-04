b2e4bb55a10af8c73468921ee550485665a2b91e
Daniel Spiewak <djspiewak@gmail.com>
2011-09-04 16:23:37 -0500

    Now capable of making a proper distribution

---
+++ make-dist.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+CWD=`pwd`
+
+mkdir /tmp/ensime-sidekick
+cp target/scala-2.9.1.final/ensimesidekick_2.9.1-0.1.jar /tmp/ensime-sidekick/EnsimeSidekick.jar
+cp $SCALA_HOME/lib/scala-library.jar /tmp/ensime-sidekick
+
+cd /tmp/ensime-sidekick
+tar cf ensime-sidekick.tar *
+
+cd $CWD
+mv /tmp/ensime-sidekick/ensime-sidekick.tar target
+rm -r /tmp/ensime-sidekick
